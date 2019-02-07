a713524910ad56e6e6be804db9a9f2217c404adb
jav26122 <jav26122@gmail.com>
2019-02-06 20:58:29 -0500

    first commit

---
+++ Lab1/hello.cpp
@@ -0,0 +1,18 @@
+
+// Jaret Varn   jav76@zips.uakron.edu
+// University of Akron
+//
+// Lab 1
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "hello world\n";
+}
+
+
+
+
+
+
---
+++ Lab2/hello.hpp
@@ -0,0 +1,12 @@
+// Jaret Varnn <jav76@uakron.edu>
+//
+// hello.hpp declares the interface to the "hello world" module.
+// This is comprised of a single function that, when called,
+// will print "hello world".
+
+#ifndef HELLO_HPP
+#define HELLO_HPP
+
+void hello();
+
+#endif
